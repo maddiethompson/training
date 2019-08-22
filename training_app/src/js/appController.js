cf83d36ca92ed1313b71f735786269289a88e894
maddiethompson <maddie.thompson@oracle.com>
2019-08-22 11:24:00 -0500

    new app creation

--- training_app/src/js/appController.js
+++ training_app/src/js/appController.js
@@ -6,16 +6,19 @@
 /*
  * Your application specific code will go here
  */
-define(['knockout', 'ojs/ojmodule-element-utils', 'ojs/ojresponsiveutils', 'ojs/ojresponsiveknockoututils', 'ojs/ojrouter', 'ojs/ojarraydataprovider', 'ojs/ojknockouttemplateutils', 'ojs/ojmodule-element', 'ojs/ojknockout'],
-  function(ko, moduleUtils, ResponsiveUtils, ResponsiveKnockoutUtils, Router, ArrayDataProvider, KnockoutTemplateUtils) {
+define(['knockout', 'ojs/ojmodule-element-utils', 'ojs/ojknockouttemplateutils', 'ojs/ojrouter', 'ojs/ojresponsiveutils', 'ojs/ojresponsiveknockoututils', 'ojs/ojarraydataprovider',
+        'ojs/ojoffcanvas', 'ojs/ojmodule-element', 'ojs/ojknockout'],
+  function(ko, moduleUtils, KnockoutTemplateUtils, Router, ResponsiveUtils, ResponsiveKnockoutUtils, ArrayDataProvider, OffcanvasUtils) {
      function ControllerViewModel() {
        var self = this;
 
-       self.KnockoutTemplateUtils = KnockoutTemplateUtils;
+       this.KnockoutTemplateUtils = KnockoutTemplateUtils;
 
       // Media queries for repsonsive layouts
       var smQuery = ResponsiveUtils.getFrameworkQuery(ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
       self.smScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
+      var mdQuery = ResponsiveUtils.getFrameworkQuery(ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP);
+      self.mdScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
 
        // Router setup
        self.router = Router.rootInstance;
@@ -58,6 +61,21 @@ define(['knockout', 'ojs/ojmodule-element-utils', 'ojs/ojresponsiveutils', 'ojs/
        iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'}
       ];
       self.navDataProvider = new ArrayDataProvider(navData, {keyAttributes: 'id'});
+
+      // Drawer
+      // Close offcanvas on medium and larger screens
+      self.mdScreen.subscribe(function() {OffcanvasUtils.close(self.drawerParams);});
+      self.drawerParams = {
+        displayMode: 'push',
+        selector: '#navDrawer',
+        content: '#pageContent'
+      };
+      // Called by navigation drawer toggle button and after selection of nav drawer item
+      self.toggleDrawer = function() {
+        return OffcanvasUtils.toggle(self.drawerParams);
+      }
+      // Add a close listener so we can move focus back to the toggle button when the drawer closes
+      document.getElementById('navDrawer').addEventListener("ojclose", document.getElementById('drawerToggleButton').focus());
 
       // Header
       // Application Name used in Branding Area
